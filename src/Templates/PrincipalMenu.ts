0a717d1d57749a248eb5e5551e6df38a27d4060e
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2022-01-28 16:51:46 -0300

    chore: card to jpg

--- src/Templates/PrincipalMenu.ts
+++ src/Templates/PrincipalMenu.ts
@@ -1,6 +1,6 @@
 
 const PricipalMenu = {
-    imageUrl: './assets/principalCard.png',
+    imageUrl: './assets/principalCard.jpg',
     textContent: `. Me chamo *João* 👨🏽‍💻 e sou o assistente virtual do *WAI* \n\n` +
     'Estamos aqui para ajudá-lo a validar sua localização  📍\n' +
     'Por favor, digite o número do CEP para que eu mostre os detalhes \n\n',
